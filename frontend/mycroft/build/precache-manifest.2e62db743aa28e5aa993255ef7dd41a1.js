9d2fb773ef258b242a9de11c1ca70ff82edf5596
Can <canx@andrew.cmu.edu>
2020-07-08 22:16:37 -0700

    resolved issues

---
+++ frontend/mycroft/build/precache-manifest.2e62db743aa28e5aa993255ef7dd41a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1410935a53f93b0465470ce6414d6665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9c3f6494a31d98959c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1d7191ef1684537ce674",
+    "url": "/static/css/main.513ac93d.chunk.css"
+  },
+  {
+    "revision": "6f9c3f6494a31d98959c",
+    "url": "/static/js/2.906aba23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.906aba23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7191ef1684537ce674",
+    "url": "/static/js/main.eb60d30c.chunk.js"
+  },
+  {
+    "revision": "e0ae3bb4f079ed15105a",
+    "url": "/static/js/runtime-main.8120c42d.js"
+  }
+]);
