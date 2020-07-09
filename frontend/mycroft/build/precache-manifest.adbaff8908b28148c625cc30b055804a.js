6004ddfc061ea8ea1b695bddf4cc8e32413d605d
Can <canx@andrew.cmu.edu>
2020-07-08 21:19:33 -0700

    add build folder

---
+++ frontend/mycroft/build/precache-manifest.adbaff8908b28148c625cc30b055804a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b13004f2cb085ea1c0fac070bd12be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9c3f6494a31d98959c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "764cb0e05c60d507b6cb",
+    "url": "/static/css/main.513ac93d.chunk.css"
+  },
+  {
+    "revision": "6f9c3f6494a31d98959c",
+    "url": "/static/js/2.906aba23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.906aba23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764cb0e05c60d507b6cb",
+    "url": "/static/js/main.f514cb24.chunk.js"
+  },
+  {
+    "revision": "e0ae3bb4f079ed15105a",
+    "url": "/static/js/runtime-main.8120c42d.js"
+  }
+]);
