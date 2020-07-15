7aefecdfafd07430f57fc28df1c988c62049dc90
Can Xia <can@whatsbusy.com>
2020-07-15 01:45:57 -0700

    build

---
+++ frontend/mycroft/build/precache-manifest.93161ec659b9b0eecbc659cc213bec40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8384489170b7ee6f985c81aa65435e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa370e993dfca231c72",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bbbe9f0cc8d512409f9f",
+    "url": "/static/css/main.513ac93d.chunk.css"
+  },
+  {
+    "revision": "4aa370e993dfca231c72",
+    "url": "/static/js/2.ba7c1363.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ba7c1363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbe9f0cc8d512409f9f",
+    "url": "/static/js/main.8bb65737.chunk.js"
+  },
+  {
+    "revision": "e0ae3bb4f079ed15105a",
+    "url": "/static/js/runtime-main.8120c42d.js"
+  }
+]);
