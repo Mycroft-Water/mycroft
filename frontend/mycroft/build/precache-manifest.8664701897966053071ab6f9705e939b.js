bb7f1e466b9faac1eac71ca903443845e3d30719
Can Xia <canx@andrew.cmu.edu>
2020-07-16 00:12:14 -0700

    build

---
+++ frontend/mycroft/build/precache-manifest.8664701897966053071ab6f9705e939b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba7848500ae797a8eeaa19c7d101425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa370e993dfca231c72",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a4c7dfe97a3e2836791",
+    "url": "/static/css/main.e502ab6a.chunk.css"
+  },
+  {
+    "revision": "4aa370e993dfca231c72",
+    "url": "/static/js/2.ba7c1363.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ba7c1363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4c7dfe97a3e2836791",
+    "url": "/static/js/main.646df7cc.chunk.js"
+  },
+  {
+    "revision": "e0ae3bb4f079ed15105a",
+    "url": "/static/js/runtime-main.8120c42d.js"
+  }
+]);
