d4e3243def84ed175a26ccfba2caf5121c1da17f
Can Xia <canx@andrew.cmu.edu>
2020-07-29 15:03:37 -0700

    updated build

--- frontend/mycroft/build/precache-manifest.9ff662eb127870f7800241dbc51102af.js
+++ frontend/mycroft/build/precache-manifest.9ff662eb127870f7800241dbc51102af.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9980ac4072a16ac6e4172163be876c25",
+    "revision": "e56889e6c9fd748eea39deadb2b329ae",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "db4939a68782b1f5f01c",
+    "revision": "a31f4db2cda7ea03636a",
     "url": "/static/css/main.e502ab6a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c39a3ccb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db4939a68782b1f5f01c",
-    "url": "/static/js/main.6ab013ea.chunk.js"
+    "revision": "a31f4db2cda7ea03636a",
+    "url": "/static/js/main.7146353d.chunk.js"
   },
   {
     "revision": "e0ae3bb4f079ed15105a",
--- frontend/mycroft/build/service-worker.js
+++ frontend/mycroft/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03b18b3cb5364df6deeab968853d2d7a.js"
+  "/precache-manifest.9ff662eb127870f7800241dbc51102af.js"
 );
 
 self.addEventListener('message', (event) => {
